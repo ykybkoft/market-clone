f6d873cffcc088792f59725576b6de5b16d73470
ykybkoft <126169447+ykybkoft@users.noreply.github.com>
2024-09-10 13:09:50 +0900

    ✨ 물건 등록 구현

---
+++ frontend/index.js
@@ -0,0 +1,75 @@
+/* <div class="item-list">
+        <div class="item-list__img">
+          <img src="assets/img.svg" />
+        </div>
+        <div class="item-list__info">
+          <div class="item-list__info-title">게이밍 PC 팝니다.</div>
+          <div class="item-list__info-meta">역삼동 19초 전</div>
+          <div class="item-list__info-price">100만원</div>
+        </div>
+      </div> */
+
+const calcTime = (timestamp) => {
+  const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+  const time = new Date(curTime - timestamp);
+
+  const hour = time.getHours();
+  const min = time.getMinutes();
+  const sec = time.getSeconds();
+  if (hour > 0) {
+    return `${hour}시간 전`;
+  } else if (min > 0) {
+    return `${min}분 전`;
+  } else {
+    return `${sec}초 전`;
+  }
+};
+
+const renderData = (data) => {
+  const main = document.querySelector("main");
+  data.reverse().forEach(async (obj) => {
+    const div = document.createElement("div");
+    div.className = "item-list";
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "item-list__img";
+
+    const img = document.createElement("img");
+    const res = await fetch(`/images/${obj.id}`);
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+    img.src = url;
+
+    const InfoDiv = document.createElement("div");
+    InfoDiv.className = "item-list__info";
+
+    const InfoTitleDiv = document.createElement("div");
+    InfoTitleDiv.className = "item-list__info-title";
+    InfoTitleDiv.innerText = obj.title;
+
+    const InfoMetaDiv = document.createElement("div");
+    InfoMetaDiv.className = "item-list__info-meta";
+    InfoMetaDiv.innerText = obj.place + " • " + calcTime(obj.insertAt);
+
+    const InfoPriceDiv = document.createElement("div");
+    InfoPriceDiv.className = "item-list__info-price";
+    InfoPriceDiv.innerText = obj.price + "원";
+
+    imgDiv.appendChild(img);
+    div.appendChild(imgDiv);
+
+    InfoDiv.appendChild(InfoTitleDiv);
+    InfoDiv.appendChild(InfoMetaDiv);
+    InfoDiv.appendChild(InfoPriceDiv);
+    div.appendChild(InfoDiv);
+    main.appendChild(div);
+  });
+};
+
+const fetchList = async () => {
+  const res = await fetch("/items");
+  const data = await res.json();
+  renderData(data);
+};
+
+fetchList();
